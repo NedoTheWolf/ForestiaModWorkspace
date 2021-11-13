9682ba920100324d58a4ede51f23c98e6dbaf280
NedoTheWolf <nedma@MSI>
2021-11-13 14:09:39 -0500

    Adding all the first stuffs and thingses

---
+++ src/main/java/net/mcreator/forestia/item/CatalystDustItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.forestia.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.forestia.ForestiaModElements;
+
+@ForestiaModElements.ModElement.Tag
+public class CatalystDustItem extends ForestiaModElements.ModElement {
+	@ObjectHolder("forestia:catalyst_dust")
+	public static final Item block = null;
+	public CatalystDustItem(ForestiaModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("catalyst_dust");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
